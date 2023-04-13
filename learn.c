9c13597b6f5d1df3244d0261fc969498dd2d3b63
Tomzyc <davidtomiwa080@gmail.com>
2023-04-13 12:12:24 -0700

    Commited all file in c

---
+++ learn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+    clear alphabelt = "A";
+    int number1 = 55;
+    float number2 = 22.34;
+    printF("char = %cln", alphabelt);
+    printF("number1 = %dln", number1);
+    printF("number2 = %fn", number2);
+    gutch()
+    clr scr()
+    return O;
+}
